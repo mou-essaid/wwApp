2a152b0f823b41655acecea79578ee27f3d4da10
mou-essaid <mouzaoui.essaid@gmail.com>
2024-04-28 14:41:48 +0100

    ggdtu

--- src/router/messageRouter.js
+++ src/router/messageRouter.js
@@ -13,7 +13,7 @@ const said = new Client({
         clientId: "said"
     }),
     puppeteer: {
-        headless: false,
+        headless: true,
         args: ['--no-sandbox', '--disable-gpu'],
     },
     webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html' }
